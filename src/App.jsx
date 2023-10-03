ba11550c0aa96585afa4c9ba406efc9e58d89b6b
Usama Rasheed <65440307+usama2188@users.noreply.github.com>
2023-10-03 17:37:51 +0500

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,31 @@
-import React from "react";
+import React from 'react'
 import './App.css'
-
 export default function App() {
 
-  const [name, setName] = React.useState("")
-  const [email, setEmail] = React.useState("")
-  const [lastName, setLastName] = React.useState("")
+
+  const [value, setValue] = React.useState(0)
 
 
-
-  const formsubmit = (e) => {
-    // this stops the page from reloading
-    e.preventDefault();
-    console.log('name:' + name)
-    console.log('lname:' + lastName)
-    console.log("my email is:" + email)
-    // console.log("my password is:")
-  }
-
-  // declare variable using callback function/arrow function
-
-  const fname = (e) => {
+  React.useEffect(() => {
+    console.log('sideeffect')
 
 
-    // id we use only e it will give us how many times an action has perofomed on input field
-    // if we write e.taret.value it will provide us with whatever we enter in the field
-    // here e is just an alphabet you can use any to call it
-    // console.log(e.target.value)
-
-    // for making a value enter in the fname we do
-    // setName(e.target.value)
-    // console.log(e.target.value)
+  }, [value])
 
 
-  }
-  const lname = (e) => {
-    // console.log(e.target.value)
-  }
-
-  const email1 = (e) => {
-    // console.log(e.target.value)
-  }
-  const passcode = (e) => {
-    // console.log(e.target.value)
-
-  }
-
+  // React.useLayoutEffect(() => {
+  //   console.log('helo')
+  // }, [])
 
   return (
-    // in the return i take input fields of a form
-    // these arrows is the main div and it contains divs inside it bcz we cannot make more than one main div
-    <>
+    <div>
+      <button onClick={() => setValue(value - 1)}  >-</button>
+      <div>{value}</div>
+      <button onClick={() => setValue(value + 1)} >+</button>
 
-      <form onSubmit={(e) => formsubmit(e)}>
+      <div style={{ width: '3rem', height: '4rem', backgroundColor: 'red' }}>
 
-        <div className='heading'>
-          <h1>form</h1>
-        </div>
-
-
-        <div className='input-1'>
-          <label htmlFor="">fname</label>
-          <input type="text" onChange={(e) => setName(e.target.value)} />
-        </div>
-
-
-
-        <div className='input-1'>
-          <label htmlFor="">lname</label>
-          <input type="text" onChange={(e) => setLastName(e.target.value)} />
-        </div>
-
-        <div className='input-2'>
-          <label htmlFor="">e-mail</label>
-          <input type="email" onChange={(e) => setEmail(e.target.value)} />
-        </div>
-
-        
-
-        <div className="button">
-          <button type='submit'>login</button>
-        </div>
-      </form>
-
-
-
-
-
-
-
-
-
-
-    </>
+      </div>
+    </div>
   )
-
 }
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
-import './style.css'
-
-
-
+import './App.css'
 
 export default function Home() {
+
+  const [creds, setCreds] = React.useState('')
+
+  const handleChange = (e) => {
+
+    setCreds((creds)=>({...creds,[e.target.name]:e.target.value}))
+  }
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(creds)
+  }
+
+
+
   return (
-    <>
-      
-      <div>
-        Home
-      </div>
-    </>
+    <div >
+      <form onSubmit={(e) => handleSubmit(e)}>
+
+        <input placeholder='name' type='text' name='name' onChange={(e) => handleChange(e)} />
+        <br />
+        <input placeholder='phone' type='text' name='phone' onChange={(e) => handleChange(e)} />
+        <br />
+        <input placeholder='email' type='text' name='email' onChange={(e) => handleChange(e)} />
+<input type="text" placeholder='password' onChange={(e)=> handleChange(e)} />
+        <button type='submit' >submit</button>
+      </form>
+    </div>
   )
 }
